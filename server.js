b86b40a8543ea58d767152c193a6e71f498fb053
Justin Bresee <justinbresee@gmail.com>
2020-02-19 21:13:43 -0700

    things are working now with the environment variables, however the frontend is not getting loaded

--- server.js
+++ server.js
@@ -5,9 +5,17 @@ const mongoose = require("mongoose");
 const port = process.env.PORT || 5000;
 const path = require("path");
 
-const mongo_URI = process.env.MONGO_URI;
+// serve static assets if we're in production
+if (process.env.NODE_ENV === "Production") {
+  // set a static folder
+  app.use(express.static("client/build"));
 
-mongoose.connect(mongo_URI, {
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
+mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true
@@ -30,16 +38,6 @@ app.use("/api/universities", universityRoute);
 app.use("/api/projects", projectRoute);
 app.use("/api/project-categories", projectCategoryRoute);
 app.use("/api/companies", companyRoute);
-
-// serve static assets if we're in production
-if (process.env.NODE_ENV === "Production") {
-  // set a static folder
-  app.use(express.static("client/build"));
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
 
 app.listen(port, () => {
   console.log("listening of port: " + port);
